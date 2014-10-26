b21f99fc4e6410960c2c51d3227e274b9987644d
auracuervog <aura.cuervo.g10@gmail.com>
2014-10-26 11:43:30 -0500

    1.0 empezando

---
+++ Punto2/particle_in_field.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+int main(int argc, char **argv){
+  double eko;
+  double alpha;
+  double masa_proton, q_proton;
+
+  eko = atof(argv[1]); //MeV
+  alpha = atof(argv[2]);
+  masa_proton = pow(1.672621777*10,-27); //MeV/c**2
+  q_proton = pow(1.602176487*10,-19); //C
+  c = 299792458; //m/s
+  vo = (eko+(masa_proton*(pow(c,2))))/sqrt(masa_proton*eko*(pow(c,2))*(eko+(2*(pow(c,2))))); 
+
+}
